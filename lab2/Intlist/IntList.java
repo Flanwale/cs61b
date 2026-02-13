7bf26e1e59b0236efc33a4664c7e27b2a2d3414f
MaZhe <a1774527542@gmail.com>
2026-02-13 19:42:11 +0800

    lab2 submit

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,18 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A == null) {
+            return B;               // 如果 A 是空，直接返回 B
+        }
+
+        IntList head = A;
+        while(A.rest!=null){
+            A = A.rest;
+        }
+
+        A.rest = B;
+
+        return head;
     }
 
     /**
@@ -91,7 +102,20 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A == null) {
+            return B;               // 如果 A 是空，直接返回 B
+        }
+        IntList res = new IntList(A.first,null);
+        IntList ptr = res;
+        A = A.rest;
+        while(A!=null){
+            ptr.rest = new IntList(A.first,null);
+            A = A.rest;
+            ptr = ptr.rest;
+
+        }
+        ptr.rest = B;
+        return res;
     }
 
 
